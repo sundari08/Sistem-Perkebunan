46ae22894b57faaa16077665a3807810bd96f6ca
sundari08 <sundari08.work@gmail.com>
2026-06-17 08:38:42 +0800

    Fix: Add FirebaseServiceProvider to decode JSON credentials from env

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,10 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    
-    // Firebase ServiceProvider (dari package)
     Kreait\Laravel\Firebase\ServiceProvider::class,
-    
-    // FirebaseServiceProvider KITA (harus setelah package)
-    App\Providers\FirebaseServiceProvider::class,
+    App\Providers\FirebaseServiceProvider::class, // ← Tambahkan
 ];
