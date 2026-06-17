ae8f24c50ddf5b14f430785d439294753604bead
sundari08 <sundari08.work@gmail.com>
2026-06-17 14:22:44 +0800

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\AdminController;
 // ========== ROUTE LOGIN ==========
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::get('/', function () {
+    return redirect()->route('login');
+}); 
 
 // ========== ROUTE LOGOUT ==========
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
